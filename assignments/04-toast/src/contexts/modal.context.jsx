8079c0f0efea6e96f9a0e0da6353eda9e96122b4
LinQuixote <leein9404@naver.com>
2024-05-30 21:55:25 +0900

    feat : 토스트 구현 기본 셋팅

---
+++ assignments/04-toast/src/contexts/modal.context.jsx
@@ -0,0 +1,43 @@
+import { produce } from "immer";
+import { createContext, useContext, useState } from "react";
+
+// context 초기값 설정
+const initialValue = {
+  add: () => {},
+  close: () => {},
+};
+// context를 만들고 provider를 컴포넌트로 분리함으로써 리랜더링때에 createContext의 재실행을 막는다.
+const ModalContext = createContext(initialValue);
+export function ModalProvider({ children }) {
+  // 무엇을 상태로 관리해야 하는가?
+  const [toastList, setToastList] = useState([]);
+
+  const value = {
+    add: (element) => {
+      setToastList((prevList) => {
+        return produce(prevList, (draft) => {
+          draft.push(element);
+        });
+      });
+    },
+    close: (id) => {
+      setToastList((prevList) => prevList.filter((toast) => toast.id !== id));
+    },
+  };
+
+  return (
+    <ModalContext.Provider value={value}>
+      {children}
+      {toastList.length !== 0 && (
+        <ul>
+          {toastList.map((toast) => (
+            <li key={toast.id}>{toast}</li>
+          ))}
+        </ul>
+      )}
+    </ModalContext.Provider>
+  );
+}
+
+// 위에서 생성한 context를 이용하여 토스트 훅을 생성
+export const useModal = () => useContext(ModalContext);
